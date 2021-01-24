fcf29febe6590a4b1fd1c1bad557f86768d7f9e1
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-24 06:19:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 function largestSubarraySum(arr) {
-  let re = /(-.)/
   console.log(arr)
-  return search(arr, re, 0)
+  return search(arr, true, 0)
 
 }
 
-function search(arr, re, idx) {
-    let x = arr.join(" ").replace(re, 0).split(" ")
-    if (idx + 1 == arr.length) return arr
-    console.log("IDX: ", idx)
-    if (parseInt(x[idx]) <= 0) {
-      x.shift()
-      console.log("ee", x)
-      return search(x, re, idx)
-    } else {
-      return search(x, re, idx + 1)
-    }
-    console.log("Final: ", x)
+
+function search(arr, canAdd, idx) {
+  let prev = arr[idx - 1]
+  let cur = arr[idx]
+  let next = arr[idx + 1]
+
+  if ((cur < 0 && canAdd)) {
+    search(arr, false, idx + 1)
+  }
+  if (cur > 0 && !canAdd) {
+    search(arr, canAdd, idx + 1)
+  }
+  
 }
+
+// function search(arr, re, idx) {
+//     let possibleAdd = []
+//     let negatives = []
+//     arr.each (num => {
+//       if (negatives.length > 0) {
+//         if (num < 0) {
+//           negatives.push(num)
+//         } else {
+//           possibleAdd.push(num)
+//         }
+//       } 
+//       else {
+//           if (num < 0) {
+//             negatives.push(num)
+//           } else {
+//             possibleAdd.push(num)
+//           }
+//       }
+//     })
+// }
 
 let array = [-2, -3, 4, -1, -2, 1, 5, -3]
 console.log(largestSubarraySum(array))
