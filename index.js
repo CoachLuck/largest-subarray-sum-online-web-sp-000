2dcc6fe7a679aaa8c3574f5251a3a248b3ae8dd6
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-24 04:15:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function largestSubarraySum(arr) {
   var max = Math.max.apply(null, arr)
-  return max
+  console.log("Highest: ", max)
+  arr.splice(arr.indexOf(max), 1)
+  var max2 = Math.max.apply(null, arr)
+  console.log("Second: ", max2)
+  return max + max2
 }
 
 let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
