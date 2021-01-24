561d6c03d2a1f1a23c2d7ce0f93af0caa65f4f79
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-24 05:53:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function largestSubarraySum(arr) {
 
 function search(arr, re, idx) {
     let x = arr.join(" ").replace(re, 0).split(" ")
-    console.log(x)
+    console.log("X: ", x)
     if (parseInt(x[idx]) < 0) {
       x.shift()
       console.log("ee", x)
