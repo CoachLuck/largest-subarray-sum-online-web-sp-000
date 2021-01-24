2d96cbb1e511b0df45c74fb98dd997cf5c9e30e8
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-24 04:25:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function largestSubarraySum(arr) {
   arr.sort()
   arr = arr.filter(x => { return x > -1 })
-  return arr.reduce(function(t, n) {
-    return t + n
-  })
+  return arr.reduce((t, n) => { return t + n} )
 }
 
 let array = [1, 100, 4, 15, 9, 30]
